be1b794da9510e14413a03a749b9c18c91f91b06
zachneb <113885140+zachneb@users.noreply.github.com>
2025-04-29 00:01:43 -0400

    Add files via upload

---
+++ email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $to = "bzpritchard@yahoo.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+    
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message could not be sent.";
+    }
+}
+?>
